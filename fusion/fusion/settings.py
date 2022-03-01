0442cac8547dfea9603258132cfec7a47da8b68a
Werichinilson De Abreu Ataliba <wataliba@brq.com>
2022-03-01 14:57:59 -0300

    save  hanges

--- fusion/fusion/settings.py
+++ fusion/fusion/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -86,11 +86,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 """
+
 DATABASES = {
     'default': dj_database_url.config()
-}"""
+}
 
 
 # Password validation
