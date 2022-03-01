38def664129e512507788d867b2186b39200e891
Werichinilson De Abreu Ataliba <wataliba@brq.com>
2022-03-01 15:55:17 -0300

    save changes

--- fusion/fusion/settings.py
+++ fusion/fusion/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'core',
+    'django_adminlte',
+    'django_adminlte_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
