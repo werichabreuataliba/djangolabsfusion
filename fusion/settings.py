325fbd2093a64407803bb149154ea39ac1bc0fdc
Werichinilson De Abreu Ataliba <wataliba@brq.com>
2022-03-01 12:41:21 -0300

    save changes

--- fusion/settings.py
+++ fusion/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b#37%&+ljr-c^97419cc9(0$^^lznk3po5behwf0*slfdpdc@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
